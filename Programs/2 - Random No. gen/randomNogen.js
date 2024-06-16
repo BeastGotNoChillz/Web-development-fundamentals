2d87493de4ed282b88f934a4db84976c57502deb
Aditya Bam <167054902+BeastGotNoChillz@users.noreply.github.com>
2024-06-16 19:22:45 +0545

    Update randomNogen.js

--- Programs/2 - Random No. gen/randomNogen.js
+++ Programs/2 - Random No. gen/randomNogen.js
@@ -11,12 +11,12 @@ console.log(randomNum);
 */
 
 const roll = document.getElementById("roll");
-const label = document.getElementById("label");
+const result = document.getElementById("result");
 const min = 1;
 const max = 6;
 let randomNum;
 
-roll.onlick = function(){
-    randomNum = Math.trunc(Math.random * (max - min)) + min;
-    label.textContent = randomNum;
+roll.onclick = function(){
+    randomNum = Math.floor(Math.random()* (max - min + 1)) + min ;
+    result.textContent = `You got ${randomNum}`;
 }
